09536754c6360a92e82980fb069f0aff4ba4f4bb
fulprimitiv <eremeevdima51@gmail.com>
2026-09-25 15:40:35 +0500

    feat: Добавлен марквиз в разметку

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import "./styles/style.css";
+
+function getUserName() {
+  const user = window.WebApp?.initDataUnsafe?.user;
+  return [user?.first_name, user?.last_name].filter(Boolean).join(" ");
+}
+
+const name = getUserName();
+console.log("name:", name);
